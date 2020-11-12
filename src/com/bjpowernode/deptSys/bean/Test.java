036d23d3137a92a825a7a1ff4f0703ab30121e40
zhangliang <2655904102@qq.com>
2020-11-12 16:51:30 +0800

    添加Test.java

---
+++ src/com/bjpowernode/deptSys/bean/Test.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.deptSys.bean;
+
+/**
+ * @ProjectName: deptSys
+ * @Package: com.bjpowernode.deptSys.bean
+ * @Description: java类作用描述
+ * @Author: Andy
+ * @CreateDate: 2020/11/12 16:49
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class Test {
+
+    private int id;
+
+}
