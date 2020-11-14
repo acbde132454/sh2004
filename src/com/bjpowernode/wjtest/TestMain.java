fb833f735b0ebb38c0b2cfb6aca7db31a8c4d2f8
wj-willem <haowen_xx@126.com>
2020-11-14 14:53:45 +0800

    添加tomcat lib包

---
+++ src/com/bjpowernode/wjtest/TestMain.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.wjtest;
+
+/**
+ * @author wj_willem
+ * @version 1.0
+ * @Description
+ * @since 2020-11-14 14:49
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("我来了，你该走了");
+    }
+}
