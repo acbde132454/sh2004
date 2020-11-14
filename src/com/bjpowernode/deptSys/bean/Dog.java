418c906842744b5b6cb6eba4cd15722e37b139b9
zhangsan <2655904102@qq.com>
2020-11-14 14:54:27 +0800

    添加Dog类

---
+++ src/com/bjpowernode/deptSys/bean/Dog.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.deptSys.bean;
+
+/**
+ * @ProjectName: test
+ * @Package: com.bjpowernode.deptSys.bean
+ * @Description: java类作用描述
+ * @Author: Andy
+ * @CreateDate: 2020/11/14 14:52
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class Dog {
+    private String name;
+    private int age;
+
+}
