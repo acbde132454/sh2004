9bb1e540442329baf6137d074a5b96cdb890f507
zhangsan <2655904102@qq.com>
2020-11-14 14:57:43 +0800

    添加Train类

---
+++ src/com/bjpowernode/deptSys/bean/Train.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.deptSys.bean;
+
+/**
+ * @ProjectName: test
+ * @Package: com.bjpowernode.deptSys.bean
+ * @Description: java类作用描述
+ * @Author: Andy
+ * @CreateDate: 2020/11/14 14:57
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class Train {
+}
